277d6b5e30f304b5d389d45226cb3b766b1e7c5f
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-02-27 23:59:34 -0700

    creating list  of files
    Added node-walk to crawl 'files' dir and methods to call that.

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-/* GET Model List */
-router.get('/models', function(req, res) {
-  var models = '';
-})
-
 module.exports = router;
--- routes/models.js
+++ routes/models.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var path = require('path');
+var walk = require('walk');
 
 var router = express.Router();
+
+var files = [];
+
+// Walker options
+
+var walker = walk.walk('./files', {followLinks: false});
+
+walker.on('file', function(root,stat,next) {
+  // Add this file to the list of files
+  files.push(root + '/' stat.name);
+  next();
+});
+
+walker.on('end', function() {
+  console.log(files);
+});
 
 /* GET list of models. */
 router.get('/', function(req, res) {
