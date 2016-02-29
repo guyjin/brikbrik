fa543dc5a81abe899e20e5b96633d26f044ce9c7
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-02-29 15:14:48 -0700

    added model files.
    New model files added.  Working on breaking down paths.

--- routes/models.js
+++ routes/models.js
@@ -6,28 +6,33 @@ var router = express.Router();
 
 
 
-// Build file walker to assemble list of lego models
-var files = [];
 
-// Walker options
-var walker = walk.walk('./files', {followLinks: false});
-
-// Walker Events
-walker.on('file', function(root,stat,next) {
-  // Add this file to the list of files
-  files.push(root + '/' stat.name);
-  next();
-});
-
-walker.on('end', function() {
-  console.log(files);
-});
 
 
 
 /* GET list of models. */
 router.get('/', function(req, res) {
-  res.send('respond with a list of files');
+  // Build file walker to assemble list of lego models
+  var files = [];
+  var models;
+  // Walker options
+  var walker = walk.walk('./files', {followLinks: false});
+
+  // Walker Events
+  walker.on('file', function(root,stat,next) {
+    // Add this file to the list of files
+    files.push(root + '/' + stat.name);
+    next();
+  });
+
+  walker.on('end', function() {
+    console.log("files array: ", files);
+
+    models = JSON.stringify(files);
+    console.log("models", models);
+  });
+
+  res.json(models);
 });
 
 
