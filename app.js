2449f3c6296db300f1152769702899bd0486562c
Ben Vaughan <benjamin.vaughan@gmail.com>
2016-03-14 16:58:26 -0600

    route and require changes
    Added EJS and changed to that engine.  Modded the /models route to
    generate the response in the walker.on('end') call.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,7 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -63,3 +64,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+app.listen(3000, function() {
+  console.log("Brikbrik running on port 3000");
+})
--- routes/models.js
+++ routes/models.js
@@ -14,25 +14,29 @@ var router = express.Router();
 router.get('/', function(req, res) {
   // Build file walker to assemble list of lego models
   var files = [];
-  var models;
+  var models = '';
   // Walker options
   var walker = walk.walk('./files', {followLinks: false});
 
   // Walker Events
   walker.on('file', function(root,stat,next) {
     // Add this file to the list of files
-    files.push(root + '/' + stat.name);
+    files.push(stat.name);
     next();
   });
 
   walker.on('end', function() {
     console.log("files array: ", files);
-
     models = JSON.stringify(files);
     console.log("models", models);
+    res.set({
+      'Content-type': 'application/json'
+    })
+    res.json(models);
+
   });
 
-  res.json(models);
+
 });
 
 
